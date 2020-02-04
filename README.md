9b75326cfe02a1d153da60dff91ce2c7d7977c3c
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2020-02-04 12:34:27 +0200

    Add: init setup and basic Caddyfile for backend/frontend domains

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 ## Description
 This repo is made for this [article](#) in which I explain how to setup local development environment that runs over HTTPS using [Caddy](https://caddyserver.com/docs/) as reverse-proxy server.
 
+## Getting Started
+### Prerequisites
+> Please review [article](#) for the installation.
+  * mkcert tool
+  * caddy server (v2)
+
+### Run Project
+* Clone project
+```bash
+git clone https://github.com/devahmedshendy/dev-over-https-traditional-setup.git
+```
+
+* Install backend dependencies and run it
+> - Open new terminal.  
+> - Switch to dev-over-https-traditional-setup/backend.
+```bash
+npm install
+
+npm run dev
+```
+
+* Install frontend dependencies and run it  
+> - Open new terminal.  
+> - Switch to dev-over-https-traditional-setup/frontend.
+```bash
+npm install
+
+npm run serve
+```
+
+* Start Caddy server
+> - Open new terminal.  
+> - Switch to dev-over-https-traditional-setup/caddy.
+```bash
+caddy run
+```
---
+++ init.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Description:
+#   This script is to setup caddy server for running 
+# both node.js & vue.js apps locally over HTTPS (https://*.foo.bar)
+#
+#   - create certificate authority 
+#   - create trusted certificate for *.foo.bar domain.
+# 
+
+echo """
+> Install local Certificate Authority
+  mkcert -install
+"""
+
+mkcert -install
+
+echo """
+> Create trusted certificate for *.foo.bar domain
+  mkcert "*.foo.bar"
+"""
+
+mkcert "*.foo.bar"
+
+echo """
+> Save generated certs under ./caddy/certs
+  cp ./_wildcard.foo.bar.pem ./caddy/certs
+  cp ./_wildcard.foo.bar-key.pem ./caddy/certs
+"""
+
+mv ./_wildcard.foo.bar.pem ./caddy/certs
+mv ./_wildcard.foo.bar-key.pem ./caddy/certs
+
+
+echo """
+> Gant low-numbered port access to caddy server
+  sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/caddy
+"""
+
+sudo setcap CAP_NET_BIND_SERVICE=+eip $(which caddy)
+
+echo """
+> Please add the following to /etc/hosts
+  127.0.0.1 backend.foo.bar frontend.foo.bar
+"""
+
